160b291a6530c60b66f8e99970a2a8b3783cb787
Armazok <nikolai.borsin@mail.ru>
2023-02-05 13:58:26 +0500

    Add: Deploy Refactor

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'ecad3ae6-8083-4154-b722-6cea4b99f288'
+        'API-KEY': '9d03aa1a-06f6-4c73-82af-a4fc37af8ec1'
     }
 })
